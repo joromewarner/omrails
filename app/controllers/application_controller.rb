a2f1030e20992176555f7209ef56159d7ef3e14b
Jorome Warner <warnerjorome@gmail.com>
2017-08-17 15:08:34 -0400

    Made User an Admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def access_denied(execption)
+    redirect_to root_path, alert: execption.message
+  end
 end
