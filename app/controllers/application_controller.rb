8372e8f683a21e36bcaacdbe5ef82ea8015e5c9b
Jorome Warner <warnerjorome@gmail.com>
2017-08-17 15:44:44 -0400

    Admin Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def access_denied(execption)
-    redirect_to root_path, alert: execption.message
+  def access_denied(exception)
+    redirect_to root_path, alert: exception.message
   end
 end
