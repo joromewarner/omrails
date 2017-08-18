df5ddcd9c2baaf4c62262cc5af8b17d54dbd33e5
Jorome Warner <warnerjorome@gmail.com>
2017-08-17 22:20:02 -0400

    Change Admin Interface5

--- app/models/admin_authorization.rb
+++ app/models/admin_authorization.rb
@@ -1,6 +1,6 @@
 class AdminAuthorization < ActiveAdmin::AuthorizationAdapter
 
-  def authorized?(action, subject = false)
+  def authorized?(action, subject = nil)
     user && user.admin?
   end
 
